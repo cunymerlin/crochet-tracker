f7b924f6827a5f7087bb23f034af1ce3f6ec8e7a
cunymerlin <esmerlin.lorareyes@lc.cuny.edu>
2026-05-22 11:17:18 -0400

    config: update API URL to production

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000";
+const API = "https://crochet-tracker-api.onrender.com";
 
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;0,800;1,400&family=Quicksand:wght@300;400;500;600&display=swap');
